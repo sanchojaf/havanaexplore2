0b042abdfc64a40e332d3de9f8c0cce1ed156d50
Aneli Valdes Acosta <anelivaldes@gmail.com>
2016-04-26 14:53:19 -0400

    Added home page

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,16 @@
 Rails.application.routes.draw do
+  get 'home/index'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
-  # You can have the root of your site routed with "root"
-  # root 'welcome#index'
 
-  # Example of regular route:
-  #   get 'products/:id' => 'catalog#view'
+  # You can have the root of your site routed with "root"
+   root 'home#index'
+
+  controller :home do
+    get 'home' => :index
+  end
 
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
